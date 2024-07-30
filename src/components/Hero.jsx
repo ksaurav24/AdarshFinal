0fa26e4c53bd9e29619844bb33098606de388ef7
Saurav <138647854+ksaurav24@users.noreply.github.com>
2024-07-30 20:42:41 +0530

    The scale change in BG-IMAGE

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,9 @@ const Hero = () => {
   return (
     <>
       <motion.div
-        className="w-screen absolute bg-cover top-0  overflow-hidden h-screen -z-10"
+        className="w-screen absolute bg-cover top-0  overflow-hidden h-screen  -z-10"
         initial={{ scale: 1 }}
-        animate={{ scale: 1.3 }}
+        animate={{ scale: 1.1 }}
         transition={{ duration: 2, ease: "easeInOut" }}
       >
         <Image
@@ -18,7 +18,7 @@ const Hero = () => {
           className="w-screen absolute bg-cover top-0  overflow-hidden h-screen -z-10"
         />
       </motion.div>
-      <div className="w-screen bg-red-700  absolute -z-10  mix-blend-color-burn h-[87vh]"></div>
+      <div className="w-screen bg-red-700  absolute -z-10  mix-blend-color-burn h-[92vh]"></div>
       <div className="w-screen h-[87vh] flex flex-col justify-center items-center"></div>
     </>
   );
