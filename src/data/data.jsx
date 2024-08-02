cdb838985c4fbea46aa2e9988732bb9d429669dd
Saurav <138647854+ksaurav24@users.noreply.github.com>
2024-08-02 19:16:39 +0530

    Image Try

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,6 +1,12 @@
 import qualityIcon from "../../public/qualityIcon.svg";
 import teamIcon from "../../public/teamIcon.svg";
 import supportIcon from "../../public/supportIcon.svg";
+import ServiceAc from "../../public/ServiceAC.webp";
+import ServiceFridge from "../../public/ServiceFridge.webp";
+import ServiceMicrowave from "../../public/ServiceMicrowave.webp";
+import ServiceRo from "../../public/ServiceRo.webp";
+import ServiceWm from "../../public/ServiceWm.webp";
+import ServiceWd from "../../public/ServiceWd.webp";
 
 const navLinks = [
   {
@@ -50,31 +56,37 @@ const services = [
     title: "Air conditioner",
     description:
       "Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    image: ServiceAc,
   },
   {
     title: "Washing Machine",
     description:
       "Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    image: ServiceWm,
   },
   {
     title: "Fridge",
     description:
       "Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    image: ServiceFridge,
   },
   {
     title: "Microwave",
     description:
       "Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    image: ServiceMicrowave,
   },
   {
     title: "RO Water Purifier",
     description:
       "Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    image: ServiceRo,
   },
   {
     title: "Water Dispenser",
     description:
       "Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    image: ServiceWd,
   },
 ];
 
