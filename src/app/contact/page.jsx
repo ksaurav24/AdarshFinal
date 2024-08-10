30a8bf5b342d4963a07b53b84e42d16e45ac148e
Saurav <138647854+ksaurav24@users.noreply.github.com>
2024-08-10 18:53:53 +0530

    Length of loading

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -135,7 +135,7 @@ const Page = () => {
               }}
               className={`${
                 isLoading
-                  ? "disabledBtn bg-gray-600 hover:bg-gray-600 cursor-not-allowed lg:w-[8vw] w-[70vw]"
+                  ? "disabledBtn bg-gray-600 hover:bg-gray-600 cursor-not-allowed lg:w-[8vw] w-[40vw]"
                   : "lg:w-[14vw] w-[90vw] bg-red-600 hover:bg-red-800"
               }   overflow-hidden flex justify-center items-center  disabled:bg-gray-600 disabled:hover:bg-gray-600   text-zinc-50   h-12 rounded-md cursor-pointer  transition-all duration-500 ease-in-out`}
             >
