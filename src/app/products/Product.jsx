dec9ff39edf07f888c1a4b5b6a7cde767c43faf9
Saurav <138647854+ksaurav24@users.noreply.github.com>
2024-09-18 15:05:59 +0530

    Refactor page.jsx: Remove unused metadata object and add "use client" import statement to page.jsx

---
+++ src/app/products/Product.jsx
@@ -0,0 +1,65 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
+
+const Product = ({ product, idx }) => {
+  const [showOverlay, setShowOverlay] = useState(false);
+  return (
+    <a
+      key={idx}
+      {...(product.link && {
+        href: product.link,
+      })}
+    >
+      <div
+        onClick={() => {
+          setShowOverlay(!showOverlay);
+        }}
+        className="lg:w-[22vw] w-[80vw] rounded-lg hover:border-zinc-500 hover:-translate-y-2 transition-all duration-300 ease-in-out border-[2px] border-transparent h-[360px] bg-white shadow-xl"
+      >
+        <div className="w-full p-6 h-[200px] overflow-hidden relative">
+          <Image
+            src={product.image}
+            alt={`${product.title}_Image`}
+            className="h-[90%] mx-auto object-contain"
+          />
+          <AnimatePresence>
+            {showOverlay && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                {...(product.link && { animate: { opacity: 1 } })}
+                exit={{ opacity: 0 }}
+                className="absolute flex flex-col justify-center items-center gap-2 top-0 left-0 w-full h-full bg-zinc-800/20 "
+              >
+                <h3 className="px-4 py-2 rounded-md text-lg font-medium text-center bg-zinc-950 text-zinc-50">
+                  {product.link ? "Buy Now" : "Contact Us for Price"}
+                </h3>
+                <a
+                  href={`https://wa.me/+919811391053/text=I+want+to+buy+this+product+${encodeURIComponent(
+                    product.title
+                  )}`}
+                  className=" px-4 py-2 rounded-md text-lg font-medium text-center bg-zinc-950 text-zinc-50"
+                >
+                  Whatsapp Us
+                </a>
+                <a
+                  href={`tel:+919811391053`}
+                  className="px-3 py-1 rounded-md text-lg font-medium text-center bg-zinc-50 text-zinc-950"
+                >
+                  Call Us
+                </a>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+        <div className="p-4 flex flex-col rounded-lg bg-zinc-100 w-full h-[156px] ">
+          <h3 className="text-sm font-regular h-[126px]">{product.title}</h3>
+          <h4 className="text-md font-semibold"> {product.price}</h4>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default Product;
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -4,14 +4,13 @@ import { products } from "../../data/data";
 import { Saira } from "next/font/google";
 import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
+import Product from "./Product";
 
 const saira = Saira({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 const Page = () => {
-  const [showOverlay, setShowOverlay] = useState(false);
-
   return (
     <div id="products" className="w-screen py-12 bg-white flex flex-col ">
       <h1
@@ -29,63 +28,7 @@ const Page = () => {
         className="grid lg:grid-cols-4 grid-cols-1 mx-auto gap-6 mt-4"
       >
         {products.Fridge.map((product, idx) => {
-          return (
-            <a
-              key={idx}
-              {...(product.link && {
-                href: product.link,
-              })}
-            >
-              <div
-                onClick={() => {
-                  setShowOverlay(!showOverlay);
-                }}
-                className="lg:w-[22vw] w-[80vw] rounded-lg hover:border-zinc-500 hover:-translate-y-2 transition-all duration-300 ease-in-out border-[2px] border-transparent h-[360px] bg-white shadow-xl"
-              >
-                <div className="w-full p-6 h-[200px] overflow-hidden relative">
-                  <Image
-                    src={product.image}
-                    alt={`${product.title}_Image`}
-                    className="h-[90%] mx-auto object-contain"
-                  />
-                  <AnimatePresence>
-                    {showOverlay && (
-                      <motion.div
-                        initial={{ opacity: 0 }}
-                        {...(product.link && { animate: { opacity: 1 } })}
-                        exit={{ opacity: 0 }}
-                        className="absolute flex flex-col justify-center items-center gap-2 top-0 left-0 w-full h-full bg-zinc-800/20 "
-                      >
-                        <h3 className="px-4 py-2 rounded-md text-lg font-medium text-center bg-zinc-950 text-zinc-50">
-                          {product.link ? "Buy Now" : "Contact Us for Price"}
-                        </h3>
-                        <a
-                          href={`https://wa.me/+919811391053/text=I+want+to+buy+this+product+${encodeURIComponent(
-                            product.title
-                          )}`}
-                          className=" px-4 py-2 rounded-md text-lg font-medium text-center bg-zinc-950 text-zinc-50"
-                        >
-                          Whatsapp Us
-                        </a>
-                        <a
-                          href={`tel:+919811391053`}
-                          className="px-3 py-1 rounded-md text-lg font-medium text-center bg-zinc-50 text-zinc-950"
-                        >
-                          Call Us
-                        </a>
-                      </motion.div>
-                    )}
-                  </AnimatePresence>
-                </div>
-                <div className="p-4 flex flex-col rounded-lg bg-zinc-100 w-full h-[156px] ">
-                  <h3 className="text-sm font-regular h-[126px]">
-                    {product.title}
-                  </h3>
-                  <h4 className="text-md font-semibold"> {product.price}</h4>
-                </div>
-              </div>
-            </a>
-          );
+          return <Product product={product} idx={idx} />;
         })}
       </div>
       <h3
@@ -98,25 +41,7 @@ const Page = () => {
         className="grid lg:grid-cols-4 grid-cols-1 mx-auto gap-6 mt-4"
       >
         {products.AC.map((product, idx) => {
-          return (
-            <a key={idx} href={product.link}>
-              <div className="lg:w-[22vw] w-[80vw] rounded-lg hover:border-zinc-500 hover:-translate-y-2 transition-all duration-300 ease-in-out border-[2px] border-transparent h-[360px] bg-white shadow-xl">
-                <div className="w-full p-6 h-[200px] overflow-hidden relative">
-                  <Image
-                    src={product.image}
-                    alt={`${product.title}_Image`}
-                    className="h-[90%] mx-auto object-contain"
-                  />
-                </div>
-                <div className="p-4 flex flex-col rounded-lg bg-zinc-100 w-full h-[156px] ">
-                  <h3 className="text-sm font-regular h-[126px]">
-                    {product.title}
-                  </h3>
-                  <h4 className="text-md font-semibold"> {product.price}</h4>
-                </div>
-              </div>
-            </a>
-          );
+          return <Product product={product} idx={idx} />;
         })}
       </div>
       <h3
@@ -129,25 +54,7 @@ const Page = () => {
         className="grid lg:grid-cols-4 grid-cols-1 mx-auto gap-6 mt-4"
       >
         {products.Microwave.map((product, idx) => {
-          return (
-            <a key={idx} href={product.link}>
-              <div className="lg:w-[22vw] w-[80vw] rounded-lg hover:border-zinc-500 hover:-translate-y-2 transition-all duration-300 ease-in-out border-[2px] border-transparent h-[360px] bg-white shadow-xl">
-                <div className="w-full p-6 h-[200px] overflow-hidden relative">
-                  <Image
-                    src={product.image}
-                    alt={`${product.title}_Image`}
-                    className="h-[90%] mx-auto object-contain"
-                  />
-                </div>
-                <div className="p-4 flex flex-col rounded-lg bg-zinc-100 w-full h-[156px] ">
-                  <h3 className="text-sm font-regular h-[126px]">
-                    {product.title}
-                  </h3>
-                  <h4 className="text-md font-semibold"> {product.price}</h4>
-                </div>
-              </div>
-            </a>
-          );
+          return <Product product={product} idx={idx} />;
         })}
       </div>
       <h3
@@ -160,25 +67,7 @@ const Page = () => {
         className="grid lg:grid-cols-4 grid-cols-1 mx-auto gap-6 mt-4"
       >
         {products.Washing_Machine.map((product, idx) => {
-          return (
-            <a key={idx} href={product.link}>
-              <div className="lg:w-[22vw] w-[80vw] rounded-lg hover:border-zinc-500 hover:-translate-y-2 transition-all duration-300 ease-in-out border-[2px] border-transparent h-[360px] bg-white shadow-xl">
-                <div className="w-full p-6 h-[200px] overflow-hidden relative">
-                  <Image
-                    src={product.image}
-                    alt={`${product.title}_Image`}
-                    className="h-[90%] mx-auto object-contain"
-                  />
-                </div>
-                <div className="p-4 flex flex-col rounded-lg bg-zinc-100 w-full h-[156px] ">
-                  <h3 className="lg:text-sm text-xs  font-regular h-[126px]">
-                    {product.title}
-                  </h3>
-                  <h4 className="text-md font-semibold"> {product.price}</h4>
-                </div>
-              </div>
-            </a>
-          );
+          return <Product product={product} idx={idx} />;
         })}
       </div>
       <h3
@@ -191,26 +80,7 @@ const Page = () => {
         className="grid lg:grid-cols-4 grid-cols-1 mx-auto gap-6 mt-4"
       >
         {products.RO.map((product, idx) => {
-          return (
-            <a key={idx} href={product.link}>
-              <div className="lg:w-[22vw] w-[80vw] rounded-lg hover:border-zinc-500 hover:-translate-y-2 transition-all duration-300 ease-in-out border-[2px] border-transparent h-[360px] bg-white shadow-xl">
-                <div className="w-full p-6 h-[200px] overflow-hidden relative">
-                  <Image
-                    src={product.image}
-                    alt={`${product.title}_Image`}
-                    className="h-[95%] mx-auto object-contain"
-                    unoptimized={true}
-                  />
-                </div>
-                <div className="p-4 flex flex-col rounded-lg bg-zinc-100 w-full h-[156px] ">
-                  <h3 className="text-sm font-regular h-[126px]">
-                    {product.title}
-                  </h3>
-                  <h4 className="text-md font-semibold"> {product.price}</h4>
-                </div>
-              </div>
-            </a>
-          );
+          return <Product product={product} idx={idx} />;
         })}
       </div>
     </div>
